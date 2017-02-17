309fe16756584e9fca7f4ea500114198b0284624
kgit1 <kgit1@rambler.ru>
2017-02-17 03:05:45 +0200

    up

--- src/a1/Start/MainTest.java
+++ src/a1/Start/MainTest.java
@@ -7,14 +7,23 @@ public class MainTest {
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("context.xml");
 
-		Person p = ctx.getBean("john", Person.class);
-		Animal a = ctx.getBean("doge", Animal.class);
+		Person p1 = ctx.getBean("john", Person.class);
+		Animal a1 = ctx.getBean("doge1", Animal.class);
 
-		System.out.println(p.getName());
-		System.out.println(a.getName());
-		System.out.println(p.getPet().getAge());
-		System.out.println(p);
-		System.out.println(a);
+		System.out.println(p1.getName());
+		System.out.println(a1.getName());
+		System.out.println(p1.getPet().getAge());
+		System.out.println(p1);
+		System.out.println(a1);
+
+		Person p2 = ctx.getBean("ariana", Person.class);
+		System.out.println(p2);
+
+		Animal a2 = ctx.getBean("doge2", Animal.class);
+		System.out.println(a2);
+		
+		Animal a3 = ctx.getBean("doge3", Animal.class);
+		System.out.println(a3);
 
 	}
 }
