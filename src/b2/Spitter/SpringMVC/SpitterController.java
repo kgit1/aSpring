057f836c4cb59d84e9dc814855bacc8f0dc5197d
kgit1 <kgit1@rambler.ru>
2017-03-01 23:55:13 +0200

    up

---
+++ src/b2/Spitter/SpringMVC/SpitterController.java
@@ -0,0 +1,12 @@
+package b2.Spitter.SpringMVC;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+//show to spring that this is controller class
+@Controller
+// root mapping for all controllers in this file
+@RequestMapping("/spitter")
+public class SpitterController {
+
+}
